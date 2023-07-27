af350810bc1ac958529e65969e5793b67ee0d272
unknown <vasanthgamer2000@gmail.com>
2023-07-27 16:33:08 +0530

    Changes has been Committed

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phase1Section4._13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            runApp();
+            Console.ReadKey();
+        }
+
+        public static void runApp()
+        {
+            int[] marks = new int[10] { 56, 90, 76, 88, 82, 67, 98, 83, 67, 79 };
+
+            Console.WriteLine("Enter marks to search and press Enter:");
+            string input = Console.ReadLine();
+            int search = Int32.Parse(input);
+
+            for (int i = 0; i < marks.Length; i++)
+            {
+                if (marks[i] == search)
+                {
+                    Console.WriteLine(marks[i] + " was found at location " + i);
+                    break;
+                }                
+            }            
+        }
+    }
+}
